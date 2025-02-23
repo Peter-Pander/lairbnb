a3aaba47a9a5e2545a44afe57b4e1f079d9a95b5
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-02-23 18:52:42 +0100

    Implement basic show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,8 +2,14 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
-end
 
-def flat_params
-  params.require(:flat).permit(:photo, :name, :description, :amenities, :price_per_night, :address)
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:photo, :name, :description, :amenities, :price_per_night, :address)
+  end
 end
