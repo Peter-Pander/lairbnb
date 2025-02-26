2f2ecb89970f16dfa444b61e2a670c2da9539536
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-02-26 14:24:52 +0100

    Zoom map more out

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -40,6 +40,6 @@ export default class extends Controller {
 
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
 
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 4, duration: 0 })
   }
 }
