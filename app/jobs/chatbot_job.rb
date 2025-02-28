3e171eed9acdd9a0150e284b550ac76948dd4999
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-02-28 05:40:05 +0100

    Add the AI part

---
+++ app/jobs/chatbot_job.rb
@@ -0,0 +1,29 @@
+# app/jobs/chatbot_job.rb
+class ChatbotJob < ApplicationJob
+  queue_as :default
+
+  def perform(question)
+    @question = question
+    chatgpt_response = client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: questions_formatted_for_openai # to code as private method
+      }
+    )
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    question.update(ai_answer: new_content)
+    Turbo::StreamsChannel.broadcast_update_to(
+      "question_#{@question.id}",
+      target: "question_#{@question.id}",
+      partial: "questions/question", locals: { question: question })
+  end
+
+  private
+
+  def client
+    @client ||= OpenAI::Client.new
+  end
+
+  # [...]
+end
