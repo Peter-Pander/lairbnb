be00f3c9e5bd1bae42b9e8c16dcac81c4c310a81
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-02-28 05:14:51 +0100

    Create #index Action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,7 @@
+# app/controllers/questions_controller.rb
 class QuestionsController < ApplicationController
+  def index
+    @questions = current_user.questions
+    @question = Question.new # for form
+  end
 end
