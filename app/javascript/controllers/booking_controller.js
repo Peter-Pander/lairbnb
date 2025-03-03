49e68c4b633a187695999f406174ad87593dcde3
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-03-03 03:46:01 +0100

    Convert script into Stimulus controller on flat show page

---
+++ app/javascript/controllers/booking_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["startDatetime", "endDatetime", "guests", "finalPrice", "priceBreakdown"];
+
+  connect() {
+    this.pricePerNight = parseFloat(this.element.dataset.bookingPricePerNight); // Get price from data attribute
+  }
+
+  calculateTotal() {
+    const startDate = this.startDatetimeTarget.value;
+    const endDate = this.endDatetimeTarget.value;
+    const guests = parseInt(this.guestsTarget.value);
+
+    if (startDate && endDate && guests > 0) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      const timeDifference = end - start;
+      const nights = timeDifference / (1000 * 3600 * 24); // Convert ms to days
+
+      if (nights > 0) {
+        const totalPrice = this.pricePerNight * nights * guests;
+
+        // Update all elements with the final price
+        this.finalPriceTargets.forEach(element => {
+          element.innerHTML = `${totalPrice} gold`;
+        });
+
+        // Price breakdown
+        this.priceBreakdownTarget.innerHTML = `${this.pricePerNight} gold x ${nights} nights x ${guests} guests`;
+
+      } else {
+        this.finalPriceTargets.forEach(element => {
+          element.innerHTML = `Invalid date range`;
+        });
+        this.priceBreakdownTarget.innerHTML = ''; // Clear breakdown
+      }
+    } else {
+      this.finalPriceTargets.forEach(element => {
+        element.innerHTML = '';
+      });
+      this.priceBreakdownTarget.innerHTML = ''; // Clear breakdown
+    }
+  }
+}
