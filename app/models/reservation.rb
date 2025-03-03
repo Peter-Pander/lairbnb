e6ef2aeed9cb2e9178f5b392f19befa62930f54b
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-03-03 07:55:05 +0100

    Create the Reservation Model

---
+++ app/models/reservation.rb
@@ -0,0 +1,19 @@
+class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :flat
+
+  # Validations
+  validates :start_datetime, :end_datetime, :guests, presence: true
+  validate :end_after_start
+
+  private
+
+  # Custom validation to ensure the end date is after the start date
+  def end_after_start
+    return if start_datetime.blank? || end_datetime.blank?
+
+    if end_datetime <= start_datetime
+      errors.add(:end_datetime, "must be after the check-in date")
+    end
+  end
+end
