b841d137a955fa7a91f963e22e3bc569943c51ef
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-03-31 02:13:50 +0200

    Fix user-login and related problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :skip_flash_messages
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     root_path # Redirects user to the homepage after signing in
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
   def skip_flash_messages
     flash.delete(:notice) if flash[:notice] == "Signed in successfully." || flash[:notice] == "Signed out successfully."
     flash.delete(:alert) if flash[:alert].present?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- app/javascript/controllers/photo_controller.js
+++ app/javascript/controllers/photo_controller.js
@@ -1,10 +1,16 @@
-// app/javascript/controllers/photo_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["fileInput"]
+  static targets = ["fileInput"];
 
+  // Triggered when the user clicks the "Edit" button.
   editPhoto() {
     this.fileInputTarget.click();
+  }
+
+  // Triggered when a file is selected.
+  upload() {
+    // Automatically submit the form when a file is chosen.
+    this.fileInputTarget.closest("form").submit();
   }
 }
